5229cb29308e5689a47e7d27de337f385f42b47f
OdinM13 <markodinius@gmail.com>
2025-09-26 23:03:39 +0200

    Build solution for 01-07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    stringArr = [];
+    stringLen = string.length;
+    for (let i = 0; i <= stringLen; i++){
+        stringArr.push(string[stringLen - i]);
+    }
+    return stringArr.join("");
 };
 
 // Do not edit below this line
