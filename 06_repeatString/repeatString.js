0af50bc53c84f09eea4e7cd9b9e7920c539c6858
OdinM13 <markodinius@gmail.com>
2025-09-26 22:41:58 +0200

    Build solution for 01-06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let stringArr = [];
+    for (let i = 0; i < number; i++){
+        stringArr.push(string);
+    }
+    return stringArr.join("");
 };
 
 // Do not edit below this line
