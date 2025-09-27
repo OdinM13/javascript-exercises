742e99534a266987a1f642abdfcbe1d467c92f2d
OdinM13 <markodinius@gmail.com>
2025-09-27 22:59:58 +0200

    Build solution for 01-08 case 1 and 2

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,30 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, removeElement, ...moreElements) {
+    // if (arguments.length !== 2) return "ERROR!";
+    if (moreElements.length === 0){
+        // const found = array.findIndex((element) => element === removeElement);
+        //console.log(found);
+        // const removed = array.splice(found, 1);
+        // return array;
+        array = removeSimple(array, removeElement);
+    } else {
+        for (let i = 0; i < moreElements.length; i++) {
+            const found = array.findIndex((element) => element === moreElements[i]);
+            //console.log(found);
+            const removed = array.splice(found, 1);
+            // return array;
+            array = removeSimple(array, removeElement);
+        }
+    }
+    return array;
 };
+
+function removeSimple(array, simpleElement) {
+        const found = array.findIndex((element) => element === simpleElement);
+        //console.log(found);
+        const removed = array.splice(found, 1);
+        return array;
+}
+console.log(removeFromArray([1, 2, 2, 3], 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
