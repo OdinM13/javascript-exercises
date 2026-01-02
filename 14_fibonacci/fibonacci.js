d03ff78251fb6217b382552d71bbd750f9558fcb
OdinM13 <markodinius@gmail.com>
2026-01-02 17:53:10 +0100

    Build solution for problem set 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    const number = Number(n);
+    const conditionOne = 0;
+    const conditionTwo = 1;
+    
+    if (number < 0){
+        return "OOPS";
+    } else if (number === conditionOne) {
+        return conditionOne;
+    } else if (number === conditionTwo) {
+        return conditionTwo;
+    } else {
+        return fibonacci(number-1) + fibonacci(number-2);
+    }
 
 };
 
