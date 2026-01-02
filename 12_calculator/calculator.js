851fb949bb6b3b1c327b11aab39a7ccecc066610
OdinM13 <markodinius@gmail.com>
2026-01-02 15:48:51 +0100

    Build solution for problem set 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(x, y) {
+    return x + y;	
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+    return x - y;	
 };
 
-const sum = function() {
-	
+const sum = function(x) {
+    if (x.length < 1) {
+        return 0;
+    } else {
+    return x.reduce((total, value) => total + value);
+    }
 };
 
-const multiply = function() {
-
+const multiply = function(x) {
+    if (x.length < 2) {
+        return 0;
+    } else {
+    return x.reduce((total, value) => total * value);
+    }
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+    return x ** y;	
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+    let sum = x;
+    if (x < 2) {
+        return 1;
+    } else {
+        while (x > 1) {
+            sum = sum * (x - 1);
+            x--;
+        }
+    return sum;
+    }
 };
 
 // Do not edit below this line
