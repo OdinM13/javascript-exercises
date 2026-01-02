8ce8ed72cb0b8676c68261aa936d3463101f733b
OdinM13 <markodinius@gmail.com>
2026-01-02 17:13:58 +0100

    Build solution for problem set 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,29 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const regex = /[ ?.!,]/g; 
+    const shortString = string.replaceAll(regex, '').toLowerCase();;
+    const stringArray = shortString.split('');
+    const arrayLength = stringArray.length;
+    const lengthRoundedDown = Math.floor(arrayLength);
+    let tracking = 0;
 
+    for (let i = 0; i <= lengthRoundedDown; i++) {
+        if (stringArray[i] === stringArray[arrayLength - 1 - i]) {
+            tracking += 0;
+        } else {
+            tracking += 1;
+        }
+    }
+
+    // console.log(shortString);
+    // console.log(stringArray);
+    // console.log(arrayLength);
+    // console.log(lengthRoundedDown);
+
+    if (tracking === 0){
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
