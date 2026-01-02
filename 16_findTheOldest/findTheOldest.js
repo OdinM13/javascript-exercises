c28ce54f4b2e5a39252d6455ee54838bf12f46e0
OdinM13 <markodinius@gmail.com>
2026-01-02 20:58:28 +0100

    Build solution for problem set 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const getAge = (birth, death) => {
+    if (!death) death = new Date().getFullYear();
+    return death - birth;
+};
 
+const findTheOldest = function(array) {
+    const sortedArray = array.sort((a, b) => {
+        const lastAge = getAge(a.yearOfBirth, a.yearOfDeath);
+        const nextAge = getAge(b.yearOfBirth, b.yearOfDeath);
+        return nextAge - lastAge; // Sorting descending (oldest first)
+    });
+
+    // You need to return the first object of the sorted array
+    return sortedArray[0];
 };
 
 // Do not edit below this line
