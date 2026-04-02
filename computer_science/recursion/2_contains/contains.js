834fbef88485e1f5b2a19ce0ae39333eb0699d44
OdinM13 <markodinius@gmail.com>
2026-04-02 20:59:02 +0200

    Build solution for 2_contains

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,18 @@
-const contains = function() {
-  
+const contains = function(object, element) {
+  for (const property in object) {
+    // Object.is is used because it can handle "Nan"
+    if (Object.is(object[property], element)) {
+      return true;
+    } else {
+      if (typeof object[property] === 'object') {
+        if (contains(object[property], element)) {
+          return true;
+        }
+      } 
+    } 
+  } 
+  return false;
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
