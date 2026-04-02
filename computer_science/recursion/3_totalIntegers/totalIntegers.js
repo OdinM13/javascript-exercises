99aed9bc77bf31a7a303bcfdbe78f9b120437378
OdinM13 <markodinius@gmail.com>
2026-04-02 21:51:10 +0200

    Build solution for 3_totalIntegers

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,17 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function(object, count = 0) {
+  if (typeof object !== 'object') {
+    return undefined;
+  }
+  for (const property in object) {
+    if (Number.isInteger(object[property])) {
+      count += 1;
+    }
+    if (typeof object[property] === 'object') {
+      count = totalIntegers(object[property], count);
+    }
+  } 
+  return count;
 };
-  
+
 // Do not edit below this line
 module.exports = totalIntegers;
