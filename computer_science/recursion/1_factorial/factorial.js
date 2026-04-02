69cf93e2fdfd8bcc042552de9eadd03af308ff26
OdinM13 <markodinius@gmail.com>
2026-04-02 19:33:46 +0200

    Build solution for 1_factorial

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,11 @@
-const factorial = function() {
-
+const factorial = function(n) {
+  if (n < 0 || typeof n === "string" || n % 1 !== 0 || typeof n === "object") {
+    return undefined;
+  }
+  if (n === 0) {
+    return 1;
+  }
+  return n * factorial(n-1);
 };
 
 // Do not edit below this line
