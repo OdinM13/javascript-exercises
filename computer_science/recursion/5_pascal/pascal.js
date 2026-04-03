ff2a9b96677b3d27d91c681e501269af3547ea81
OdinM13 <markodinius@gmail.com>
2026-04-03 22:06:15 +0200

    Build solution for 5_pascal

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,24 @@
-const pascal = function() {
+const pascal = function(int) {
+  const output = [];
+
+  const generate = (n, k) => {
+    if (k === n || k === 0) {
+      return 1;
+    }
+    return generate(n - 1, k - 1) + generate (n - 1, k);
+  }
+
+  for (let i = 0; i < int; i++) {
+    if (i === 0 || i === int - 1) {
+      output.push(1);
+    } else {
+      output.push(generate(int - 1, i));
+    }
+  }
   
+  return output;
 };
-  
+
+
 // Do not edit below this line
 module.exports = pascal;
